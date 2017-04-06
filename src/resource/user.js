3206b880557e6a0c47b51d54b6f4188b8b4f56bd
LaneEvans <laneevans96@gmail.com>
2017-04-06 14:18:18 -0500

    In Class

---
+++ src/resource/user.js
@@ -0,0 +1,36 @@
+module.exports = {
+  create: create,
+  update: update,
+  read: read
+}
+
+var json = require('./../form-json');
+var encryption = require('./../lib/encryption');
+
+function create(req, res, db) {
+  json(req, res, function(req, res) {
+    var user = req.body;
+    var salt = encryption.salt();
+    var cryptedPassword = encyption.digest(user.password + salt);
+    db.run('INSERT INTO users (eid, email, firstName, lastName, cryptedPassord, salt) VALUES (?,?,?,?,?,?)', [
+      user.eid,
+      user.email,
+      user.firstName,
+      user.lastName,
+      cryptedPassword,
+      salt
+    ], function(err) {
+      if(err) {return;}
+      res.statusCode(200);
+      res.end("User Created");
+    });
+  })
+}
+
+function read(req, res, db) {
+  var id = req.params.id;
+    db.get('SELECT eid, email, firstName, lastName FROM user WHERE id=?', [id], function(user) {
+      res.setHeader("Content-Type", "text/json");
+      res.end(JSON.stringify(user));
+    });
+}
